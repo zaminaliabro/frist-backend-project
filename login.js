8b2f9561574df548336fd6b54843d7e0659e91b5
zamin-ali <191748592+zaminaliabro@users.noreply.github.com>
2026-01-30 22:57:48 +0500

    Update login.js

--- login.js
+++ login.js
@@ -8,21 +8,39 @@ const password = document.getElementById("password");
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
+  if (!email.value || !password.value) {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  if (password.value.length <= 6) {
+    alert("Password must be at least 6 characters long.");
+    return;
+  }
+
   const user = finder1(email.value);
 
-  if (user && user.password === password.value) {
+  if (!user) {
+    alert("User not found ");
+    return;
+  }
+
+  if (user.password === password.value) {
     const currentUser = {
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email,
     };
 
+    alert("Login successful ");
+
     const tokenObj = createTokenWithExpiry(10, 1);
     saveToken(tokenObj);
     console.log("Token created:", tokenObj);
 
     localStorage.setItem("currentUser", JSON.stringify(currentUser));
-
     window.location.href = "user.html";
+  } else {
+    alert("Wrong password ");
   }
 });
